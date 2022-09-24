bfd31c61c384cf7462559023d6e6087af2546bca
DJayanga <iit17021@std.uwu.ac.lk>
2022-09-24 14:33:27 +0530

    Navbar Responived

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,25 +1,29 @@
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 
 import "./index.css";
 import Logo from "../../images/logo.png";
 import { links } from "../../data";
 import { GoThreeBars } from "react-icons/go";
+import {MdOutlineClose} from "react-icons/md"
 
 const Navbar = () => {
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <nav>
       <div className="container nav-container">
-        <Link to="/" className="logo">
+        <Link to="/home" className="logo">
           <img src={Logo} alt="Nav Logo" />
         </Link>
-        <ul className="nav-links">
+        <ul className= {`nav-links ${isNavOpen ? 'show-nav' : "hide-nav"}`}>
           {links.map(({ name, path }, index) => {
             return (
               <li>
                 <NavLink
                   to={path}
                   className={({ isActive }) =>
-                    isActive ? "active-nav" : ""
+                    isActive ? "active-nav" : undefined
                   }
                 >
                   {name}
@@ -28,8 +32,10 @@ const Navbar = () => {
             );
           })}
         </ul>
-        <button className="nav-toggle-btn">
-          <GoThreeBars />
+        <button className="nav-toggle-btn" onClick={() => {
+           setIsNavOpen(!isNavOpen);
+        }}>
+          {isNavOpen? <MdOutlineClose /> : <GoThreeBars />}
         </button>
       </div>
     </nav>
